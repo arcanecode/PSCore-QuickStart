4df537cf8a89e330a83ab6545149f73759d23640
ArcaneCode <arcanecode@ArcaneMac13.local>
2019-11-21 12:12:44 -0600

    Testing new password, no real change in code

--- Demos/01 - Cmdlets.ps1
+++ Demos/01 - Cmdlets.ps1
@@ -9,6 +9,7 @@
   This sample is Copyright (c) 2016, 2018 Robert C. Cain. All rights reserved.
   The code herein is for demonstration purposes. No warranty or guarentee
   is implied or expressly granted. 
+  
   This module may not be reproduced in whole or in part without the express
   written consent of the author. 
 -----------------------------------------------------------------------------#>
